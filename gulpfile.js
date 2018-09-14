b8965b58bcbec16a37c13496c5dfdae10ccde006
Danielle <dani.code.camp@gmail.com>
2018-09-14 00:20:49 -0400

    Organized CSS files, started using BEM for styles, set up BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+    nested = require('postcss-nested'), 
+    cssImport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
     console.log('Hooray! You created a Gulp task!');
@@ -15,17 +17,30 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))    
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))    
         .pipe(gulp.dest('./app/temp/styles/'));
 });
 
 gulp.task('watch', function() {
 
-     watch('./app/index.html', function() {
-        gulp.start('html');
-     });
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        } 
+    });
 
-     watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-     });
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+
+});
+
+gulp.task('cssInject', ['styles'] ,function() {
+    return gulp.src('./app/temp/styles/style.css')
+        .pipe(browserSync.stream());
 });
